0e55b4d6a333bf93a5850aba545ef33040e79204
Aadil7874 <socialaadil470@gmail.com>
2024-12-10 11:00:25 +0530

    Req/Res Controller

--- app.js
+++ app.js
@@ -1,5 +1,20 @@
 import http from "http"
 // const require = require("http")
-const server = http.createServer();
+
+const controller = (req, res) => {
+    //   console.log(req.body);
+    //   res.statusCode = 400;
+    //   res.end("Hello World!");
+    if (req.url.includes("welcome")) {
+        res.statusCode = 200;
+        res.end("Welcome to My Backend Server!");
+    } else {
+        res.statusCode = 404;
+        res.end();
+    }
+
+}
+
+const server = http.createServer(controller);
 const port = 5000;
 server.listen(port, () => console.log("Server is UP & Running!"))
