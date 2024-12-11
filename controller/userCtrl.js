4a45b1d35ce978fc437a4d76a62d858b918c2123
Aadil7874 <socialaadil470@gmail.com>
2024-12-12 01:38:24 +0530

    Middleware, Encoding, Hashing & JWT

---
+++ controller/userCtrl.js
@@ -0,0 +1,68 @@
+import userModel from "../models/usersModel.js";
+import { hashPassword, comparePassword, generateToken } from "../utils/utils.js";
+const signup = async (req, res) => {
+    try {
+        const data = req.body
+        const user = new userModel(data)
+        user.createdAt = new Date()
+        user.isActive = true
+        user.password = await hashPassword(data.password)
+        await user.save()
+        res.status(201).json({ message: "User Added Succesfully!", success: true })
+    } catch (err) {
+        res.status(500).json({ message: "Internal Server Error!", success: false })
+    }
+}
+const login = async (req, res) => {
+    try {
+        const { email, password } = req.body;
+        const user = await userModel.findOne({ email }).lean();
+        if (user) {
+            const isPasswordMatches = await comparePassword(password, user.password)
+            if (isPasswordMatches) {
+                const token = generateToken({ email, name: user.name, id: user._id })
+                return res.status(200).json({
+                    message: "User LoggedIn Successfully!", success: true, data: {
+                        name: user.name,
+                        email: user.email,
+                        _id: user._id,
+                        token,
+                    }
+                })
+            }
+        }
+        res.status(401).json({ message: "Incorrect email or password!", succes: false })
+
+    } catch (e) {
+        console.log(e);
+
+        res.status(500).json({ message: "Internal Server Error!", success: false })
+    }
+}
+// const login = async (req, res) => {
+//     try {
+//         const { email, password } = req.body;
+//         const user = await userModel.findOne({ email }).lean();
+//         if (user) {
+//             const isPasswordMatches = comparePassword(password, user.password);
+//             if (isPasswordMatches) {
+//                 const token = generateToken({ email, name: user.name, id: user._id });
+//                 return res.status(200).json({
+//                     message: "User Loggedin Successfully!",
+//                     success: true,
+//                     data: {
+//                         name: user.name,
+//                         email: user.email,
+//                         _id: user._id,
+//                         token,
+//                     },
+//                 });
+//             }
+//         }
+//         res.status(401).json({ message: "Incorrect Email or Password!", success: false });
+//     } catch (err) {
+//         console.log(err);
+//         res.status(500).json({ message: "Internal Server Error!", success: false });
+//     }
+// };
+export { signup, login };
--- utils/utils.js
+++ utils/utils.js
@@ -1,3 +1,6 @@
+import bcrypt from "bcrypt"
+import jwt from "jsonwebtoken"
+
 const getOptions = (req) => {
     const search = req.query.search || "";
     const pageSize = parseInt(req.params.pageSize) || 10;
@@ -18,4 +21,41 @@ const getOptions = (req) => {
         pageSize,
     };
 };
-export { getOptions };
+const authenticate = (req, res, next) => {
+    // console.log(req.headers.authorization);
+    if (req.headers.authorization) {
+        const encodedStr = req.headers.authorization.split(" ")[1]
+        const decodedStr = Buffer.from(encodedStr, "base64").toString()
+        const [email, password] = decodedStr.split(":")
+        if (email === "admin" && password === "123456") {
+            next()
+        } else {
+            res.status(401).send({ message: "Unauthorized from authenticate utils middleware! 0", success: false })
+        }
+    } else {
+        res.status(401).send({ message: "Unauthorized from authenticate utils middleware!", success: false })
+    }
+}
+const hashPassword = (password) => {
+    return bcrypt.hash(password, 2);
+};
+const comparePassword = (plainPass, hashedPass) => {
+    return bcrypt.compare(plainPass, hashedPass);
+}
+const generateToken = (data) => {
+    return jwt.sign(data, process.env.SECRET_KEY, { expiresIn: 60 });
+};
+const verifyToken = (req, res, next) => {
+    const token = req.headers.authorization.split(" ")[1];
+    jwt.verify(token, process.env.SECRET_KEY, (err, decoded) => {
+        console.log("decoded", decoded);
+        if (err) {
+            console.log(err);
+            res.status(401).send({ message: "Unauthorized! from jwtVerify", success: false });
+        } else {
+            req.decoded = decoded;
+            next();
+        }
+    });
+};
+export { getOptions, authenticate, hashPassword, comparePassword, generateToken, verifyToken };
